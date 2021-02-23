b5d56e71cf2b04f4a61ba22d498b0531e27f6ec4
juanjovr <58473253+juanjovr@users.noreply.github.com>
2021-02-23 12:50:44 +0100

    Add_line

---
+++ Practica1/prueba1/prueba1.ino
@@ -0,0 +1,18 @@
+void setup() {
+  
+
+  pinMode(13,OUTPUT);
+  boolean estado;
+
+}
+
+void loop() {
+  Serial.println(digitalWrite(13,HIGH));
+  delay(1000);
+  Serial.println(digitalWrite(13,LOW));
+  delay(1000);
+
+  //Prueba de Github
+  
+
+}
