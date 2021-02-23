5cf5123be3b469516b3919b517d73b4aab70c73f
juanjovr <58473253+juanjovr@users.noreply.github.com>
2021-02-23 12:53:43 +0100

    nuevos_cambios

--- Practica1/prueba1/prueba1.ino
+++ Practica1/prueba1/prueba1.ino
@@ -10,9 +10,10 @@ void loop() {
   Serial.println(digitalWrite(13,HIGH));
   delay(1000);
   Serial.println(digitalWrite(13,LOW));
-  delay(1000);
+  
 
   //Prueba de Github
+  //Segunda prueba
   
 
 }
